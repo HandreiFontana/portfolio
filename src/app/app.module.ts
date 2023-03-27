f8c911b2664058927e6f59fc838a1655097d00b0
HandreiFontana <handreifontana@gmail.com>
2023-03-27 11:20:14 -0300

    refact: changed component toolbar to folder components/header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ToolbarComponent } from './components/header/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
