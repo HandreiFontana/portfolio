4b85bac4c1a307dd24a0e1823c6b27f52c3047eb
HandreiFontana <handreifontana@gmail.com>
2023-03-27 12:13:21 -0300

    feat: profile added to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ToolbarComponent } from './components/header/toolbar/toolbar.component';
+import { ProfileComponent } from './components/header/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
