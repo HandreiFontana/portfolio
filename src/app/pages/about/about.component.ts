cc1b1762ecff6b1babd24fac9624fc211f57beed
HandreiFont <handrei.fontana@vamilly.com.br>
2023-03-30 17:36:48 -0300

    feat: styles updated; about page styles updated

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core'
+import { IKeyValue } from 'src/app/interfaces/key-value'
+
+interface IResponse {
+  about?: string[]
+  tools?: IKeyValue[]
+  basic?: IKeyValue[]
+}
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public about: string[] = []
+  public tools: IKeyValue[] = []
+  public basic: IKeyValue[] = []
 
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpClient
+      .get('assets/json/about.json')
+      .subscribe({
+        next: ({ about, tools, basic }: IResponse) => {
+          this.about = about as string[]
+          this.tools = tools as IKeyValue[]
+          this.basic = basic as IKeyValue[]
+        }
+      })
+  }
 }
