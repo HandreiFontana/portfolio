d394bce55c6d658baa530c657873f750e1ede879
HandreiFont <handrei.fontana@vamilly.com.br>
2023-05-09 11:44:32 -0300

    feat: auto scroll on toolbar click added

--- src/app/components/header/toolbar/toolbar.component.ts
+++ src/app/components/header/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core'
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  public scrollY: number = 0
 
+  public navigateTo(route: string) {
+    switch (route) {
+      case 'about':
+        window.scrollTo(0, 562)
+        break
+      case '':
+        break
+    }
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    console.clear()
+    console.log(this.scrollY)
+    this.scrollY = window.scrollY
+  }
 }
