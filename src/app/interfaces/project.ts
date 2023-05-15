b4a1d510ac6a8ceeef8a4a136521c7b3b10e70d3
HandreiFontana <HandreiFontana>
2023-05-15 17:59:45 -0300

    feat: added github button to redirect to github project

--- src/app/interfaces/project.ts
+++ src/app/interfaces/project.ts
@@ -1,6 +1,7 @@
 export interface IProject {
   title?: string
   url?: string
+  github?: string
   imageUrl?: string
   technologies?: string[]
 }
--- src/assets/i18n/data-en-us/project.ts
+++ src/assets/i18n/data-en-us/project.ts
@@ -2,6 +2,7 @@ export const projects = [
   {
     title: "CRUD",
     url: "https://angularcrud.handrei.com.br",
+    github: "https://github.com/HandreiFontana/portfolio-angular-node",
     imageUrl: "assets/images/projects/angular-crud.png",
     technologies: [
       "Angular 2+",
