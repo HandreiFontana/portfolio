2ecba09f88795aa01c99c21e773821ce641eb530
HandreiFontana <HandreiFontana>
2023-05-22 13:21:14 -0300

    feat: added 100% of tests on language select component

--- src/app/components/header/language-select/language-select.component.spec.ts
+++ src/app/components/header/language-select/language-select.component.spec.ts
@@ -1,23 +1,53 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { LanguageSelectComponent } from './language-select.component';
+import { LanguageSelectComponent } from './language-select.component'
+import { LanguageService } from 'src/app/services/language.service'
 
 describe('LanguageSelectComponent', () => {
-  let component: LanguageSelectComponent;
-  let fixture: ComponentFixture<LanguageSelectComponent>;
+  let component: LanguageSelectComponent
+  let service: LanguageService
+  let fixture: ComponentFixture<LanguageSelectComponent>
+  let changeLanguageSpyOn: jasmine.Spy
+  let closeModalSpyOn: jasmine.Spy
+  let language: string
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LanguageSelectComponent ]
     })
-    .compileComponents();
+    .compileComponents()
 
-    fixture = TestBed.createComponent(LanguageSelectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(LanguageSelectComponent)
+    service = TestBed.inject(LanguageService)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+
+    language = 'pt-br'
+
+    changeLanguageSpyOn = spyOn(service, "changeLanguage")
+  })
+
+  it('should toggle modal status', () => {
+    component.toggleModal()
+    expect(component.isActiveLanguageModal).toBeTrue()
+    component.toggleModal()
+    expect(component.isActiveLanguageModal).toBeFalse()
+  })
+
+  it('should select language', () => {
+    component.selectLanguage(language)
+
+    expect(changeLanguageSpyOn).toHaveBeenCalledTimes(1)
+  })
+
+  it('should close language modal', () => {
+    component.isActiveLanguageModal = true
+    component.selectLanguage(language)
+    
+    expect(component.isActiveLanguageModal).toBeFalse()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
