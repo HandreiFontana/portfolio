05b859b7122e89e75c9553d1fbe77cba8f13caf4
HandreiFontana <HandreiFontana>
2024-02-29 16:58:25 -0300

    feat: work experience info updated

--- src/assets/i18n/data-en-us/work-experience.ts
+++ src/assets/i18n/data-en-us/work-experience.ts
@@ -1,23 +1,35 @@
 export const workExperiences = [
   {
-    company: "ClickMVP",
-    date: "October/2021 - June/2022",
-    title: "Fullstack Web Developer Intern",
+    company: 'ClickMVP',
+    date: 'October/2021 - June/2022',
+    title: 'Fullstack Web Developer Intern',
     responsabilities: [
-      "To develop Frontend application using React.JS (Material-UI)",
-      "To develop Backend application using Node.JS (Express /TypeORM)",
-      "Maintenance of PHP codes"
-    ]
+      'To develop Frontend application using React.JS (Material-UI)',
+      'To develop Backend application using Node.JS (Express /TypeORM)',
+      'Maintenance of PHP codes',
+    ],
   },
   {
-    company: "Vamilly Inovações em Soluções de TI Ltda",
-    date: "June/2022 - Moment",
-    title: "Fullstack Web Developer",
+    company: 'Vamilly Inovações em Soluções de TI Ltda',
+    date: 'June/2022 - September/2023',
+    title: 'Fullstack Web Developer I',
     responsabilities: [
-      "Head leader of projects",
-      "To develop Frontend application using Angular 2+ (PO-UI)",
-      "To develop Backend application using Node.JS (Express /TypeORM)",
-      "Mentoring Web Developers I"
-    ]
-  }
-]
+      'Head leader of projects',
+      'To develop Frontend application using Angular 2+ (PO-UI)',
+      'To develop Backend application using Node.JS (Express / TypeORM)',
+      'Mentoring Web Developers I',
+    ],
+  },
+  {
+    company: 'Conducco',
+    date: 'October/2023 - Current',
+    title: 'Fullstack Web Developer II',
+    responsabilities: [
+      'To develop Webscraping using Python (Selenium / BeautifulSoup)',
+      'To develop Workflow using Python (Airflow)',
+      'To develop Database on Snowflake',
+      'To develop Brackend application using Node.JS (Express / TypeORM)',
+      'To develop Frontend application using React (Material-UI)',
+    ],
+  },
+];
--- src/assets/i18n/data-pt-br/work-experience.ts
+++ src/assets/i18n/data-pt-br/work-experience.ts
@@ -1,23 +1,35 @@
 export const workExperiences = [
   {
-    company: "ClickMVP",
-    date: "Outubro/2021 - Junho/2022",
-    title: "Estagiário em Desenvolvimento Web Full Stack",
+    company: 'ClickMVP',
+    date: 'Outubro/2021 - Junho/2022',
+    title: 'Estagiário em Desenvolvimento Web Full Stack',
     responsabilities: [
-      "Desenvolver aplicação Frontend usando React.JS (Material-UI)",
-      "Desenvolver aplicação Backend usando Node.JS (Express /TypeORM)",
-      "Manutenção de códigos em PHP"
-    ]
+      'Desenvolver aplicação Frontend usando React.JS (Material-UI)',
+      'Desenvolver aplicação Backend usando Node.JS (Express /TypeORM)',
+      'Manutenção de códigos em PHP',
+    ],
   },
   {
-    company: "Vamilly Inovações em Soluções de TI Ltda",
-    date: "Junho/2022 - Atual",
-    title: "Desenvolvimento Web Full Stack",
+    company: 'Vamilly Inovações em Soluções de TI Ltda',
+    date: 'Junho/2022 - Setembro/2023',
+    title: 'Desenvolvimento Web Full Stack Júnior',
     responsabilities: [
-      "Líderança de projetos",
-      "Desenvolver aplicação Frontend usando Angular 2+ (PO-UI)",
-      "Desenvolver aplicação Backend usando Node.JS (Express /TypeORM)",
-      "Mentoria de Desenvolvedores Júniors"
-    ]
-  }
-]
+      'Líderança de projetos',
+      'Desenvolver aplicação Frontend usando Angular 2+ (PO-UI)',
+      'Desenvolver aplicação Backend usando Node.JS (Express /TypeORM)',
+      'Mentoria de Desenvolvedores Júniors',
+    ],
+  },
+  {
+    company: 'Conducco',
+    date: 'Outubro/2023 - Atual',
+    title: 'Desenvolvimento Web Full Stack Pleno',
+    responsabilities: [
+      'Desenvolver Webscraping em Python (Selenium / BeautifulSoup)',
+      'Desenvolver de Workflow em Python (Airflow)',
+      'Desenvolver banco de dados usando Snowflake',
+      'Desenvolver aplicação Backend usando Node.JS (Express / TypeORM)',
+      'Desenvolver aplicação Frontend usando React (Material-UI)',
+    ],
+  },
+];
