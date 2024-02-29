83159c298e43dced7e86ae6b047de45bfada6898
HandreiFontana <HandreiFontana>
2024-02-29 16:50:12 -0300

    feat: school info updated

--- src/assets/i18n/data-pt-br/education.ts
+++ src/assets/i18n/data-pt-br/education.ts
@@ -1,12 +1,19 @@
 export const educations = [
   {
-    level: "Graduação",
-    title: "Licenciatura em Matemática",
-    school: "UEPG - Universidade Estadual de Ponta Grossa",
-    date: "2019 - 2022",
+    level: 'Graduação',
+    title: 'Licenciatura em Matemática',
+    school: 'UEPG - Universidade Estadual de Ponta Grossa',
+    date: '2019 - 2022',
     details: [
-      "Láurea Acadêmica",
-      "Pesquisa Científica: Solicitações Analíticas para Equações Diferenciais"
-    ]
-  }
-]
+      'Láurea Acadêmica',
+      'Pesquisa Científica: Solicitações Analíticas para Equações Diferenciais',
+    ],
+  },
+  {
+    level: 'Mestrado',
+    title: 'Ciência da Computação',
+    school: 'UTFPR - Universidade Tecnológica Federal do Paraná',
+    date: '2024 - Andamento',
+    details: ['Inteligência Artificial'],
+  },
+];
