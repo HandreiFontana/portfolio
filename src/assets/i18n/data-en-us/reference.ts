c38b4dc49419913181dc394b71a753f5bba51942
HandreiFontana <HandreiFontana>
2024-05-24 09:31:19 -0300

    fix: andre's avatar not showing on english version

--- src/assets/i18n/data-en-us/reference.ts
+++ src/assets/i18n/data-en-us/reference.ts
@@ -5,7 +5,7 @@ export const references = [
     description:
       'Handrei é um apaixonado pelo que faz. Além da paixão pela matemática, desenvolveu conosco a paixão pelo desenvolvimento de aplicações web. Seu conhecimento hoje relativo a NodeJS (Express, TypeORM, PostgreSQL e MySQL) e ReactJS (Material-UI e Styled-components), além da experiência que teve com o dia a dia de agilidade (Scrum), o torna apto para trabalhar como desenvolvedor full-stack em qualquer empresa do mercado. Tem perfil auto-didata, aprende tudo com muita rapidez, além de ser uma pessoa que se integra facilmente em times.',
     avatar:
-      'https://media.licdn.com/dms/image/C4E03AQGgh_pQqA2PeQ/profile-displayphoto-shrink_800_800/0/1516240332545?e=1714608000&v=beta&t=9cpfCqD2lwvL7KrjQ16rquFOQNqIeVyF2Bf3kro7apU',
+    'assets/images/references/andre-dourado.jpg',
     field: 'Senior Software Engineer',
   },
 ];
